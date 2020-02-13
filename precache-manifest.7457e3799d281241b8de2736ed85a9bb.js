c879333a54ef03a2aaf5e388b1083158193502f0
Ted <heinhtetnaing186@gmail.com>
2020-02-13 18:00:33 +0630

    Updates

---
+++ precache-manifest.7457e3799d281241b8de2736ed85a9bb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f70ab694a8cf0df30fad3340a7c3a4",
+    "url": "/linda-thai/index.html"
+  },
+  {
+    "revision": "f8ba34d697f53e978560",
+    "url": "/linda-thai/static/css/main.67eb5763.chunk.css"
+  },
+  {
+    "revision": "4448b80233fa1e2ff9bd",
+    "url": "/linda-thai/static/js/2.b826f63a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/linda-thai/static/js/2.b826f63a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ba34d697f53e978560",
+    "url": "/linda-thai/static/js/main.ddc5d6cb.chunk.js"
+  },
+  {
+    "revision": "29a7c01a57522dc48762",
+    "url": "/linda-thai/static/js/runtime-main.e2bbf359.js"
+  },
+  {
+    "revision": "aa828cd45d79ac55ef2477525cddf2ea",
+    "url": "/linda-thai/static/media/Cooking.aa828cd4.svg"
+  },
+  {
+    "revision": "a4ac8abc9c3204c278f5c4eca398fe6a",
+    "url": "/linda-thai/static/media/Email.a4ac8abc.svg"
+  },
+  {
+    "revision": "39de6098012c12cde27d138e7423fd8a",
+    "url": "/linda-thai/static/media/Food.39de6098.svg"
+  },
+  {
+    "revision": "993e0f1e337ba05930ef0d59288becb8",
+    "url": "/linda-thai/static/media/Gaming.993e0f1e.svg"
+  },
+  {
+    "revision": "18066f9f4b2c53a320d9f97316025bc8",
+    "url": "/linda-thai/static/media/Linkedin.18066f9f.svg"
+  },
+  {
+    "revision": "bf62cac527e4ed97e01250f84fb9a97d",
+    "url": "/linda-thai/static/media/Music.bf62cac5.svg"
+  },
+  {
+    "revision": "3a3e6d88d9b1d55da9bbdffab18579fd",
+    "url": "/linda-thai/static/media/Pets.3a3e6d88.svg"
+  },
+  {
+    "revision": "152d6194f3027fd2ca347e8564647596",
+    "url": "/linda-thai/static/media/Phone.152d6194.svg"
+  },
+  {
+    "revision": "25b0082a1eb9dd26ece40678ea4029d3",
+    "url": "/linda-thai/static/media/Travel.25b0082a.svg"
+  },
+  {
+    "revision": "398408fdeb6a2ccfdfb94d590d66b1ac",
+    "url": "/linda-thai/static/media/image1.398408fd.png"
+  },
+  {
+    "revision": "e1d3ac618ec7da102f298fe9621e4901",
+    "url": "/linda-thai/static/media/image2.e1d3ac61.png"
+  }
+]);
